e6f5cf55b8ca1d740c67039ba84e6527abb4cfdc
ozgekuzucu <ozge.zz53@example.com>
2025-04-17 23:44:01 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class ServiceManager: IServiceService
+	{
+		IServiceDal _servicceDal;
+
+		public ServiceManager(IServiceDal servicceDal)
+		{
+			_servicceDal = servicceDal;
+		}
+
+		public void TAdd(Service t)
+		{
+			_servicceDal.Insert(t);
+		}
+
+		public void TDelete(Service t)
+		{
+			_servicceDal.Delete(t);
+		}
+
+		public Service TGetByID(int id)
+		{
+			return _servicceDal.GetByID(id);
+		}
+
+		public List<Service> TGetList()
+		{
+			return _servicceDal.GetList();
+		}
+
+		public void TUpdate(Service t)
+		{
+			_servicceDal.Update(t);
+		}
+	}
+}
