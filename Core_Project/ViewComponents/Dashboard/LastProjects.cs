5a9be69e0208fd723f8524ff60a41ef967096b98
ozgekuzucu <ozge.zz53@gmail.com>
2025-04-23 00:41:44 +0300

    .

---
+++ Core_Project/ViewComponents/Dashboard/LastProjects.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+	public class LastProjects : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
