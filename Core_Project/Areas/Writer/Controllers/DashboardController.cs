638be5b4c6fdcadad15c282674a71ea4b8ffdce9
ozgekuzucu <ozge.zz53@gmail.com>
2025-05-19 01:05:17 +0300

    .

---
+++ Core_Project/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Project.Areas.Writer.Controllers
+{
+	[Area("Writer")]
+	public class DashboardController : Controller
+	{
+		private readonly UserManager<WriterUser> _userManager;
+
+		public DashboardController(UserManager<WriterUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+			ViewBag.v = values.Name + " " + values.Surname;
+
+			//statistics
+			Context c = new Context();
+			ViewBag.v1 = 0;
+			ViewBag.v2 = c.Announcements.Count();
+			ViewBag.v3 = 0;
+			ViewBag.v4 = c.Skills.Count();
+			return View();
+		}
+	}
+}
